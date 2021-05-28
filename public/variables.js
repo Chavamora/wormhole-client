d8f6236c1546c81c280b938063671666ced886f9
Chavamora <sbetomora@gmail.com>
2021-05-28 18:15:41 -0500

    a

--- public/variables.js
+++ public/variables.js
@@ -1,6 +1,6 @@
 
 const globalVars = {
-    //apiEndPoint: 'http://localhost:3000/api',
-   apiEndPoint: 'https://wormholeplan-api.herokuapp.com/api'
+    apiEndPoint: 'http://localhost:3000/api',
+   //apiEndPoint: 'https://wormholeplan-api.herokuapp.com/api'
 }
 
